6f2ef7385b746e91412d9241a63f0481de5a3904
HasanUchiha <57262890+HasanUchiha@users.noreply.github.com>
2023-12-19 08:39:53 +0600

    using and understanding the jsx feature

--- viletry/part1/src/App.jsx
+++ viletry/part1/src/App.jsx
@@ -1,7 +1,20 @@
-const App = () => {
+const Hello = () => {
   return (
     <div>
       <p>Hello world</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div>
+      <p>Greetings</p>
+      <Hello />
+      <Hello />
+      <Hello />
+
     </div>
   )
 }
