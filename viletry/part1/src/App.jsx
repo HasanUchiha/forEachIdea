af2ba2221d79ea0f14c1dccb36369a028062fe0a
HasanUchiha <57262890+HasanUchiha@users.noreply.github.com>
2023-12-19 14:42:12 +0600

    app jsx file was modified

--- viletry/part1/src/App.jsx
+++ viletry/part1/src/App.jsx
@@ -1,21 +1,25 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
   )
 }
 
 
 const App = () => {
+  const name='hasbullah'
+  const age=19
+
   return (
     <div>
       <p>Greetings</p>
-      <Hello />
-      <Hello />
-      <Hello />
 
+      <Hello name='hasan' age ={29+7} />
+      <Hello name= {name} age ={age} />
     </div>
   )
 }
+
 export default App
